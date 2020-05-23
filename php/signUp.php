8dd8d36ab1bd6ac84e659cffbd61d40d511bfbf5
Abdelrahman Elsayed <56702823+abdel-elsayed@users.noreply.github.com>
2020-05-23 10:13:43 -0400

    Update signUp.php

--- php/signUp.php
+++ php/signUp.php
@@ -16,11 +16,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	  $phone= test_input($_POST["phone"]);
 	  $password = test_input($_POST["password"]);
 	  $conpassword = test_input($_POST["cpassword"]);
-	  
+
 	 //checking for different errors
 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)&&!preg_match("/^[a-zA-Z0-9]*$/", $username)){
 		header("Location:signUpIndex.php?error=InvalidEmailAndUsername&firstname=".$firstname."&lastname=".$lastname);
-		exit(); 
+		exit();
 	}elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
 		header("Location:signUpIndex.php?error=InvalidEmail&firstname=".$firstname."&lastname=".$lastname."username=".$username);
 		exit();
@@ -32,13 +32,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	{
 		header("Location:signUpIndex.php?error=PasswordsDontMatch&firstname=".$firstname."&lastname=".$lastname."&username=".$username. "&email=".$email);
 		exit();
+	}elseif (!preg_match("/^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$/", $password))
+	{
+		header("Location:signUpIndex.php?error=PasswordNotSecure&firstname=".$firstname."&lastname=".$lastname."&username=".$username. "&email=".$email);
+		exit();
 	}else {
 		$stmt = $conn->prepare("SELECT username, email FROM users WHERE username=? OR email=? LIMIT 1"); //To check if the username or email already exists
 		$stmt->bind_param('ss', $username, $email);
 		$stmt->execute();
 		$stmt->bind_result($username, $email);
 		$stmt->store_result();
-		if($stmt->num_rows>0) 
+		if($stmt->num_rows>0)
 			{
 				header("Location:signUpIndex.php?error=usernameTaken");
 				exit();
@@ -46,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		else { // to insert into the database using prepared statements
 		$stmt = $conn->prepare("INSERT INTO users(first_name, last_name,phone, username ,email,password) VALUES(?,?,?,?,?,?)");
 		$stmt->bind_param('ssssss',$first,$last,$phone,$username,$email,$password);
-		$stmt->execute();	
+		$stmt->execute();
 			$_SESSION['firstname'] = $first; // setting the session variables
 			$_SESSION['lastname'] = $last;
 			$_SESSION['email'] = $email;
@@ -61,7 +65,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			header("Location:profile.php");
 			exit();
 		}
-	}	
+	}
 }
 else {
 	header("Location:loginIndex.php?error=LoginFirst");
